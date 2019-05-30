5ce56b4f5349cd648c252c0b208148cf3041ea32
Maxim <zip2pac@mail.ru>
2019-05-30 20:34:18 +0300

    Create repository pattern.

---
+++ app/src/main/java/com/android/developer/testapp/data/repository/RepositoryImpl.kt
@@ -0,0 +1,6 @@
+package com.android.developer.testapp.data.repository
+
+import com.android.developer.testapp.domain.repository.Repository
+
+class RepositoryImpl : Repository {
+}
---
+++ app/src/main/java/com/android/developer/testapp/domain/repository/Repository.kt
@@ -0,0 +1,4 @@
+package com.android.developer.testapp.domain.repository
+
+interface Repository {
+}
